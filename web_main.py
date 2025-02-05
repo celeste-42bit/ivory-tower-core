e0c9d10c5f52a3ec2537e41d4ba24977ebe9b746
celeste-42bit <caeleste-42bit@proton.me>
2025-02-05 20:20:43 +0100

    Added packages: flask-sqlalchemy, requests and updated requirements.txt to reflect that change. Also, added some biolerplate code into web_main.py

---
+++ web_main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")  # index
+def index():
+    return "<p>index</p>"
+
+@app.route("/login")  # login
+def login():
+    return "<p>login</p>"
+
+@app.route("/functions/wol/<device_name>")
+def wol(device_name):
+    return f"Waking up {device_name}"
+
+@app.route("/user/<username>")  # example for dynamic routes
+def show_user(username):
+    return f"Hello, {username}"
+
+app.run(host="127.0.0.1", port=8080, debug=True)
