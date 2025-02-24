822f5d5b5abf3d7f929fc8c37f9e5ca0f5e5916a
celeste-42bit <caeleste-42bit@proton.me>
2025-02-24 22:36:05 +0100

    Updated WoL code to make it standalone, changed index.html template to display a control panel

--- web_main.py
+++ web_main.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, jsonify
+from wakeonlan import send_magic_packet
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")  # index
 def index():
-    return "<p>index</p>"
+    return render_template("index.html")
 
-@app.route("/login")  # login
-def login():
-    return "<p>login</p>"
-
-@app.route("/functions/wol/<device_name>")
+@app.route("/functions/wol/<device_name>", methods=["GET"])
 def wol(device_name):
-    return f"Waking up {device_name}"
+    try:
+        with open("./config/devices.json", "r") as f:
+            devices = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return jsonify({"error": "Internal server error: Could not load configuration"}), 500
 
-@app.route("/user/<username>")  # example for dynamic routes
+    if device_name in devices:
+        try:
+            send_magic_packet(devices[device_name])
+            return jsonify({"message": f"Magic packet sent to {device_name}. Waking up..."}), 200
+        except Exception as e:
+            return jsonify({"error": f"Exception occurred: {str(e)}"}), 500
+    else:
+        return jsonify({"error": f"Device '{device_name}' not found. Try another."}), 404
+
+
+@app.route("/user/greet/<username>", methods=["GET"])  # example for dynamic routes
 def show_user(username):
     return f"Hello, {username}"
 
-app.run(host="127.0.0.1", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
