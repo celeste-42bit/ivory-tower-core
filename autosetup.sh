7eaa1c0048f87bcbd7037ab3890eeb5894606e64
celeste-42bit <caeleste-42bit@proton.me>
2025-03-09 18:53:24 +0100

    Added annotations into installer and committed to the development path

--- autosetup.sh
+++ autosetup.sh
@@ -1,6 +1,11 @@
 python3 -m venv env
 source ./env/bin/activate
 
+#Make sure python3 and pip are installed
+    # if not, check for sudo privileges and install python3 and pip
+
+# create venv and install reqirements.txt using pip
+
 if [[ -z "$VIRTUAL_ENV" ]]; then
     exit
 fi
@@ -11,3 +16,8 @@ if [[ $? -eq 0 ]]; then
 else
     echo "Ups. Something went ded. Rip."
 fi
+
+# check if required file structure exists
+    # if not, create required directory structure and add required config files with default content
+
+# Finish and close
---
+++ development-path.md
@@ -0,0 +1,9 @@
+# Development path
+
+## Current state
+
+- WoL system is being developed, testing follows soon
+
+## Upcoming
+
+- Login requirement for use of the control panel
